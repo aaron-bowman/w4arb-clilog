cde713b46dfe5b5456a7cceb956c42a7da600a0d
Aaron Bowman <aaronrbowman7@gmail.com>
2024-04-04 14:52:03 -0400

    a lot of rework, now produces full FLE log files

--- clilog.sh
+++ clilog.sh
@@ -6,45 +6,138 @@ if [ $# -eq 0 ]; then
 fi
 
 log_file=$1
+continue="N"
 
-echo "CTRL+C to Quit"
-echo "Choose an Option"
-echo "1) Run"
-echo "2) Search & Pounce"
-read -p "Selection: " method
+## Functions
+menu() {
+  echo ""
+  echo "----- MENU -----"
+  echo "Choose an Option"
+  echo "0) Initiate Log"
+  echo "1) Contest - Run"
+  echo "2) Contest - Search & Pounce"
+  echo "3) View Log"
+  echo "---------------"
+  echo ""
+  read -p "Selection: " method
+}
+
+lookup_call(){
+  echo "Enter 'EXIT' to Return to Main Menu"
+
+  read -p "Call: " call
+
+  if [ ${call^^} = "EXIT" ]; then
+    exec bash "$0" $log_file
+  else
+    curl -sL https://api.hamdb.org/$call/json/clilog
+    echo ""
+    read -p "Continue? (Y or N) " continue
+  fi
+}
+
+
+## Begin Script
+clear
+menu
 
 if [ $method = "1" ]; then
-  echo "-----"
+
+  if ! [ -f $log_file ]; then
+    echo ""
+    echo "ERROR - Log Not Initiated..."
+    menu
+  fi
+
+  clear
+  echo "----- RUN -----"
   read -p "Frequency (MHz): " frequency
 
   while true; do
-    echo "-----"
-    read -p "Call: " call
-    curl -sL https://api.hamdb.org/$call/json/clilog
     echo ""
-    echo "-----"
+    echo "-- QSO --"
+
+    continue="N"
+
+    while ! [ ${continue^^} = "Y" ]; do
+      lookup_call
+    done
+
     read -p "Name: " name
     read -p "State: " state
+    read -p "Identifier: " identifier
+
     time=$(date -u '+%H%M')
-    echo "$frequency $time $call <$name $state>" >> $log_file
-    echo "QSO Logged: $frequency $time $call <$name $state>"
+
+    echo "$frequency $time $call <$name $state $identifier>" >> $log_file
+    echo "QSO Logged: $frequency $time $call @$name <$name $state $identifier>"
   done
 
 elif [ $method = "2" ]; then
-  while true; do
-    echo "-----"
-    read -p "Frequency (MHz): " frequency
-    read -p "Call: " call
-    curl -sL https://api.hamdb.org/$call/json/clilog
+
+  if ! [ -f $log_file ]; then
     echo ""
-    echo "-----"
+    echo "ERROR - Log Not Initiated..."
+    menu
+  fi
+
+  clear
+  echo "----- Search & Pounce -----"
+
+  while true; do
+    echo ""
+    echo "-- QSO --"
+
+    continue="N"
+
+    while ! [ ${continue^^} = "Y" ]; do
+      lookup_call
+    done
+
+    read -p "Frequency (MHz): " frequency
     read -p "Name: " name
     read -p "State: " state
+    read -p "Identifier: " identifier
+
     time=$(date -u '+%H%M')
-    echo "$frequency $time $call <$name $state>" >> $log_file
-    echo "QSO Logged: $frequency $time $call <$name $state>"
+
+    echo "$frequency $time $call <$name $state $identifier>" >> $log_file
+    echo "QSO Logged: $frequency $time $call @$name <$name $state $identifier>"
   done
 
+elif [ $method = "3" ]; then
+  clear
+  echo "----- Log Entries -----"
+  cat $log_file
+  menu
+
+elif [ $method = "0" ]; then
+
+  if [ -f $log_file ]; then
+    echo ""
+    echo "ERROR - Log Already Initiated..."
+    menu
+  fi
+
+  clear
+  echo "----- Initiating Log -----"
+  read -p "My Call: " my_call
+  read -p "My Grid: " my_grid
+  read -p "Mode: " mode
+  date=$(date -u +'%Y-%m-%d')
+  echo "# Header"          >> $log_file
+  echo "mycall $my_call"   >> $log_file
+  echo "mygrid $my_grid"   >> $log_file
+  echo "operator $my_call" >> $log_file
+  echo ""                  >> $log_file
+  echo "# Log"             >> $log_file
+  echo "date $date"        >> $log_file
+  echo "$mode"             >> $log_file
+  echo "Log Initiated:"
+  cat $log_file
+
+  menu  
 else
   echo "Try Again..."
+  menu
 fi
